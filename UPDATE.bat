4d8470204d6570153ced472e832e913897ea4836
djleo70 <83383164+djleo70@users.noreply.github.com>
2024-06-16 02:21:46 +0200

    Update UPDATE.bat

--- UPDATE.bat
+++ UPDATE.bat
@@ -1,6 +1,7 @@
 @echo off 
 chcp 65001 >nul
 title MISE A JOUR
+mode con lines=42
 echo.
 
 
@@ -68,7 +69,7 @@ endlocal
 timeout 2 >nul
 setlocal
 chcp 65001 >nul
-curl -L https://api.pastecode.io/anon/raw-snippet/p5miwe0u?raw=inline&api=true&ticket=eecd2439-867e-4893-a6b0-6a06814bdbfa -o "C:\temp\OBS_module_chat\refrenv.bat"
+curl -L https://api.pastecode.io/anon/raw-snippet/p5miwe0u?raw=attachment&api=true&ticket=eecd2439-867e-4893-a6b0-6a06814bdbfa -o "C:\temp\OBS_module_chat\refrenv.bat"
 call "C:\temp\OBS_module_chat\refrenv.bat"
 timeout 6 >nul
 for /f "tokens=*" %%i in ('where python') do set "PYTHON_PATH=%%i"
