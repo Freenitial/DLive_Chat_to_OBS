a4aca31753929b1b6da87b0b4bc82f64090872d0
djleo70 <83383164+djleo70@users.noreply.github.com>
2024-06-16 07:20:33 +0200

    Update OUVRIR CECI.bat

--- OUVRIR CECI.bat
+++ OUVRIR CECI.bat
@@ -157,8 +157,10 @@ if %errorlevel% neq 0 (
     REM Vérifier si le téléchargement a réussi
     if %errorlevel% neq 0 (
         echo Échec du téléchargement du programme d'installation de Git.
-        pause
-        exit /b 1
+        echo le script va se relancer tout seul....
+        timeout 3 > nul
+        start "" /d "%~dp0" "%~nx0"
+        exit
     )
     REM Exécuter le programme d'installation de Git en mode silencieux
     start "" /wait "C:\temp\OBS_module_chat\git-installer.exe" /VERYSILENT /NORESTART
